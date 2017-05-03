9fa7fe2f0208274c86039055851629e8acea2b63
David Martin <dwmartin41@gmail.com>
2017-05-03 20:48:13 +0100

    made 'read more' buttons invisible iff javascript is disabled

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@ var expands = document.querySelectorAll('[data-expand]');
 expands.forEach(function (e) {
     var toExpand = document.getElementById(e.getAttribute('data-expand'));
     toExpand.classList.add('expandable');
+    e.removeAttribute('data-expand');
     e.onclick = function (event) {
         toExpand.classList.add('expanded');
         this.setAttribute('style', 'display: none')
